282995ccdd99d10908934decf7293a86f1473cce
emiraliyev235 <emiraliyev235@gmail.com>
2025-04-18 19:52:46 +0400

    return

--- middleware/user.global.ts
+++ middleware/user.global.ts
@@ -1,20 +1,14 @@
-import { defineNuxtRouteMiddleware, navigateTo } from 'nuxt/app'
+import { defineNuxtRouteMiddleware } from 'nuxt/app'
 import { useUserStore } from '~/store/user.store'
 import { authApi } from '~/base/api/auth/api'
 
-export default defineNuxtRouteMiddleware(async (to) => {
+export default defineNuxtRouteMiddleware(async () => {
   const store = useUserStore()
-  const tokenCookie = useCookie('auth-token')
 
-  // Если пользователь уже авторизован, пропускаем этот middleware
   if (store.isUserLogged) return
 
-  // Если пользователь пытается попасть на /my-subs без токена, редиректим на /subs
-  if (to.path === '/my-subs' && !tokenCookie.value) {
-    return navigateTo('/subs?unauthorized=true')
-  }
+  const tokenCookie = useCookie('auth-token')
 
-  // Если токен есть, пытаемся получить данные о пользователе
   if (tokenCookie.value) {
     try {
       const userData = await authApi.getMe()
@@ -24,13 +18,6 @@ export default defineNuxtRouteMiddleware(async (to) => {
       console.error('Ошибка при получении данных пользователя:', err)
       store.setUserLogged(false)
       tokenCookie.value = null
-      // Если что-то пошло не так, редиректим на /subs с параметром unauthorized
-      return navigateTo('/subs?unauthorized=true')
     }
-  }
-
-  // Если мы находимся уже на странице /subs, не делаем редирект, чтобы избежать зацикливания
-  if (to.path === '/subs' && to.query.unauthorized) {
-    return
   }
 })
